2452172770eb477c16043f5229fa0d4dc63dba8b
Karine_Fernandes_ <163432076+Mokadd-oficial@users.noreply.github.com>
2025-07-09 13:13:55 -0300

    Update index.js

--- Extrator/routes/index.js
+++ Extrator/routes/index.js
@@ -1,1 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const CgespService = require('../services/CGESP/index.service');
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cgesp/alagamentos', async function(req, res, next) {
+  const { date } = req.query;
+  console.log('cgesp', date);
+  const cgespService = new CgespService(date);
+  await cgespService.scrapper();
+  res.json(cgespService.getObject());
+});
+
+module.exports = router;
